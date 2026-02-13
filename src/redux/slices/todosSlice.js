a8471d59dfa4fe586d6312df9e2a4fe07e17d27c
Imane-Acherrat <imaneacherrat1998@gmail.com>
2026-02-13 20:22:13 +0100

    Create todos slice + store

---
+++ src/redux/slices/todosSlice.js
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const todosSlice = createSlice({
+    name: "todos",
+    initialState: {
+        todos: [],
+        status: "idle", // idle | loading | succeeded | failed
+        error: null,
+    },
+    reducers: {
+        // Ajouter une tâche
+        addTodo: (state, action) => {
+            // On peut aussi créer l’objet todo avant le dispatch (dans le composant)
+            // et l’envoyer directement dans action.payload au lieu de le construire ici
+            const newTodo = {
+                userId: 1,
+                id: Date.now(),
+                title: action.payload,
+                completed: false,
+            };
+            state.todos.push(newTodo);
+        },
+
+        // Supprimer une tâche
+        deleteTodo: (state, action) => {
+            state.todos = state.todos.filter(
+                (todo) => todo.id !== action.payload
+            );
+        },
+
+        // Modifier le statut d'une tâche (toggle completed)
+        toggleTodo: (state, action) => {
+            const todo = state.todos.find(
+                (t) => t.id === action.payload
+            );
+            if (todo) {
+                todo.completed = !todo.completed;
+            }
+        },
+    }
+});
+
+export const { addTodo, deleteTodo, toggleTodo } = todosSlice.actions;
+export default todosSlice.reducer;
